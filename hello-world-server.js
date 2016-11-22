8ac6991f48bd41601db90c3534cca0942a17acea
canberk ovayurt <canberk.ovayurt@gmail.com>
2016-11-22 11:39:15 +0300

    test

--- hello-world-server.js
+++ hello-world-server.js
@@ -2,7 +2,7 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+ 
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,6 +18,6 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
