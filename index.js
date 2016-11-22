b60082c1cd62efaa6ba6dcd7b63fafe82b9999e8
canberk ovayurt <canberk.ovayurt@gmail.com>
2016-11-22 14:57:33 +0300

    test

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
- 
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: req.query.test }));
+     
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+	response.send(cool());
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Express server listening on port %d in %s mode", this
+			.address().port, app.settings.env);
 });
