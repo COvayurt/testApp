58855c7fee75e1fb2e2054702cac89309cdbf1af
canberk ovayurt <canberk.ovayurt@gmail.com>
2016-11-22 11:14:09 +0300

    Demo

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,13 +1,23 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-var http = require('http');
+app.set('port', (process.env.PORT || 5000));
 
-http.createServer(function handler(req, res) {
-	
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello COvayurt\nHello World!');    
-    
-}).listen('localhost');
+app.use(express.static(__dirname + '/public'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
 
-console.log('Server running at http://127.0.0.1:80/');
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
